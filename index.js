31e3d70c5d05c2988208273e98f92808cfd72f89
Yash Ahir <163372242+Yash-Ahir-5@users.noreply.github.com>
2024-05-17 14:51:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use('/api', require('./routes'));
 
-sequelize;x
+sequelize;
 
 app.use("/", require("./routes"));
 app.use("/public", express.static(path.join(__dirname, "/public/assets")));
